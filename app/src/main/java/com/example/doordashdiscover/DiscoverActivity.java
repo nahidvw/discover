b10ea8ce29fde9e8cbdbe556f982f0b7c0e5b5d6
nahidhasan <auvi.2k5@gmail.com>
2021-04-02 13:10:25 -0500

    Adding a base activity and a progressbar

--- app/src/main/java/com/example/doordashdiscover/DiscoverActivity.java
+++ app/src/main/java/com/example/doordashdiscover/DiscoverActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class DiscoverActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
