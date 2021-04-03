852df689159054184369cd959cfa761951290f3f
nahidhasan <auvi.2k5@gmail.com>
2021-04-02 23:31:13 -0500

    Setup background threads for network request

--- app/src/main/java/com/example/doordashdiscover/requests/RestaurantApiClient.java
+++ app/src/main/java/com/example/doordashdiscover/requests/RestaurantApiClient.java
@@ -3,9 +3,14 @@ package com.example.doordashdiscover.requests;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
+import com.example.doordashdiscover.AppExecutors;
 import com.example.doordashdiscover.models.Restaurant;
 
 import java.util.List;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+import static com.example.doordashdiscover.util.Constants.NETWORK_TIMEOUT;
 
 public class RestaurantApiClient {
     private static RestaurantApiClient instance;
@@ -24,5 +29,22 @@ public class RestaurantApiClient {
 
     public LiveData<List<Restaurant>> getRestaurants() {
         return mRestaurants;
+    }
+
+    public void getRestaurantsApi() {
+        final Future handler = AppExecutors.getInstance().getNetworkIO().submit(new Runnable() {
+            @Override
+            public void run() {
+                //retrieve data from REST api
+            }
+        });
+
+        AppExecutors.getInstance().getNetworkIO().schedule(new Runnable() {
+            @Override
+            public void run() {
+                //let the user know it's timed out
+                handler.cancel(true);
+            }
+        }, NETWORK_TIMEOUT, TimeUnit.MILLISECONDS);
     }
 }
--- app/src/main/java/com/example/doordashdiscover/util/Constants.java
+++ app/src/main/java/com/example/doordashdiscover/util/Constants.java
@@ -2,4 +2,5 @@ package com.example.doordashdiscover.util;
 
 public class Constants {
     public static final String BASE_URL = "https://api.doordash.com";
+    public static final int NETWORK_TIMEOUT = 3000;
 }
