1de9d9078f9ec36c5d1d3e84b603edcf77c0f05e
nahidhasan <auvi.2k5@gmail.com>
2021-04-05 23:21:37 -0500

    status test in restaurant class

--- app/src/test/java/com/example/doordashdiscover/models/RestaurantTest.java
+++ app/src/test/java/com/example/doordashdiscover/models/RestaurantTest.java
@@ -49,4 +49,36 @@ public class RestaurantTest {
         //Assert
         assertNotEquals(rest1, rest2);
     }
+
+    @Test
+    void isStatusEqual_returnTrue() {
+        //Arrange
+        Restaurant restaurant = new Restaurant();
+        restaurant.setStatus(new Status(new int[] {45,45}, true));
+
+        //Assert
+        assertEquals(restaurant.getDisplayStatus(), "45");
+
+        restaurant.setStatus(new Status(new int[] {45,45}, false));
+        assertEquals(restaurant.getDisplayStatus(), "");
+
+        restaurant.setStatus(new Status(new int[] {39}, true));
+        assertEquals(restaurant.getDisplayStatus(), "39");
+    }
+
+    @Test
+    void isStatusNotEqual_returnTrue() {
+        //Arrange
+        Restaurant restaurant = new Restaurant();
+        restaurant.setStatus(new Status(new int[] {45,45}, true));
+
+        //Assert
+        assertNotEquals(restaurant.getDisplayStatus(), "45 Mins");
+
+        restaurant.setStatus(new Status(new int[] {45,45}, false));
+        assertNotEquals(restaurant.getDisplayStatus(), "45");
+
+        restaurant.setStatus(new Status(new int[] {39}, true));
+        assertNotEquals(restaurant.getDisplayStatus(), "");
+    }
 }
